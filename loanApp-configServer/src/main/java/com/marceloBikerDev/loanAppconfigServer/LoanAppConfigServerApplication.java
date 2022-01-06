7e0e100a476ed1da2869f5e6ab1418924ba14ab8
marcelo.biker.dev@gmail.com <marcelo.biker.dev@gmail.com>
2022-01-06 20:02:23 +0000

    created service loanApp-configServer

---
+++ loanApp-configServer/src/main/java/com/marceloBikerDev/loanAppconfigServer/LoanAppConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.marceloBikerDev.loanAppconfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanAppConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanAppConfigServerApplication.class, args);
+	}
+
+}
---
+++ loanApp-configServer/src/test/java/com/marceloBikerDev/loanAppconfigServer/LoanAppConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marceloBikerDev.loanAppconfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanAppConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
