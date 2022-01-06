e4d909b5a669f49066df4b9a22c9e4b00f3547ca
marcelo.biker.dev@gmail.com <marcelo.biker.dev@gmail.com>
2022-01-06 19:42:40 +0000

    created service loanApp-lender

---
+++ loanApp-lender/src/main/java/com/marceloBikerDev/loanApplender/LoanAppLenderApplication.java
@@ -0,0 +1,13 @@
+package com.marceloBikerDev.loanApplender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanAppLenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanAppLenderApplication.class, args);
+	}
+
+}
---
+++ loanApp-lender/src/test/java/com/marceloBikerDev/loanApplender/LoanAppLenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marceloBikerDev.loanApplender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanAppLenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
