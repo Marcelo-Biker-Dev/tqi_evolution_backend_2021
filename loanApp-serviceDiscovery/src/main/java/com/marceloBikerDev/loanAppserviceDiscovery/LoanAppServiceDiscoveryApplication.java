3b611cf0b89f7ad0f8380c6a21cacfbed7c03a3b
marcelo.biker.dev@gmail.com <marcelo.biker.dev@gmail.com>
2022-01-10 14:14:32 +0000

    created service loanApp-serviceDiscovery

---
+++ loanApp-serviceDiscovery/src/main/java/com/marceloBikerDev/loanAppserviceDiscovery/LoanAppServiceDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.marceloBikerDev.loanAppserviceDiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanAppServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanAppServiceDiscoveryApplication.class, args);
+	}
+
+}
---
+++ loanApp-serviceDiscovery/src/test/java/com/marceloBikerDev/loanAppserviceDiscovery/LoanAppServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marceloBikerDev.loanAppserviceDiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanAppServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
